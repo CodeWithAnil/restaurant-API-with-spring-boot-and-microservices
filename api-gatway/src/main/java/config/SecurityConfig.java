a62a0abdc3f7e3f82bc3d0f1b468f6b1a85e52bc
anil vishwakarma <anilvishwakarma4494@gmail.com>
2024-03-04 23:54:24 +0530

    add ureka server

---
+++ api-gatway/src/main/java/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@SuppressWarnings("removal")
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity){
+        //noinspection removal
+        serverHttpSecurity.csrf()
+                .disable()
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/eureka/**")
+                        .permitAll()
+                        .anyExchange()
+                        .authenticated())
+                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
+        return serverHttpSecurity.build();
+    }
+}
